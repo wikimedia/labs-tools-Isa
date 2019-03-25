24e656188050206b545a716e93b9e2381d67ff57
Egbe Eugene <agboreugene@gmail.com>
2019-03-25 22:12:13 +0100

    Added routes and corresponding template files

--- isa/routes.py
+++ isa/routes.py
@@ -1,6 +1,26 @@
 from flask import render_template
 from isa import app
 
-@app.route("/")
+@app.route( "/" )
 def home():
     return render_template( 'home.html' )
+
+@app.route( "/campaigns" )
+def getCampaigns():
+    return render_template( 'campaigns.html' )
+
+@app.route( "/campaigns/<string:campaign_name>" )
+def getCampaignById( campaign_name ):
+    return render_template( 'campaign.html' )
+
+@app.route( "/campaigns/create" )
+def CreateCampaign():
+    return render_template( 'create_campaign.html' )
+
+@app.route( "/campaigns/<string:campaign_name>/edit" )
+def editCampaign( campaign_name ):
+    return render_template( 'edit_campaign.html' )
+
+@app.route( "/campaigns/<string:campaign_name>/entry" )
+def contributeToCampaign( campaign_name ):
+    return render_template( 'campaign_entry.html' )
