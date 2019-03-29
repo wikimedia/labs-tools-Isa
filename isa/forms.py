c997df655adb6fcfd41aa8ef5605d7149c746d32
Egbe Eugene <agboreugene@gmail.com>
2019-03-29 13:35:49 +0100

    Implemented country select for campaigns/create using pycountry

--- isa/forms.py
+++ isa/forms.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from wtforms import StringField, TextField, SubmitField, RadioField, DateField, SelectField
 from wtforms.validators import DataRequired, Length
 from wtforms.widgets import TextArea
+import pycountry
 
 Countries = [ ('-1', '-----'),
             ('rw', 'Rwanda'),
@@ -15,7 +16,9 @@ class CampaignForm( FlaskForm ):
                     validators=[DataRequired(), Length(min=2, max=20)])
     description = StringField('Description', widget=TextArea())
     categories = StringField('Categories', widget=TextArea())
-    campaign_country = SelectField('Country', choices=Countries )
+    campaign_country = SelectField(
+            'Country',
+            choices = [(country.alpha_2, country.name) for country in pycountry.countries] )
     start_date = DateField('Start date', default=datetime.utcnow )
     end_date = DateField('End date', default=datetime.utcnow )
     status = example = RadioField('Status', choices=[('True','Closed'),('Flase','Open')])
