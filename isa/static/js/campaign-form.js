41631e334a6189e87eeea2c396b663a538946937
Navino Evans <navino@histropedia.com>
2019-05-17 12:59:13 +0100

    Fix category search on Create/Update campaign pages
    Just needed to move the scripts to the correct file after recent
    splitting of JS files.
    
    Bug: T223579
    Change-Id: I924b4c97c4d374335de61722c9a2061af6778971

--- isa/static/js/campaign-form.js
+++ isa/static/js/campaign-form.js
@@ -2,4 +2,52 @@ $(document).ready( function () {
     $( function() {
         $( '.selectpicker' ).selectpicker();
     } );
+    
+    function categorySearchResultsFormat(state) {
+        if (!state.id) {
+          return state.text;
+        }
+        var $state = $( '<span class="search-result-label">' + state.text + '</span>');
+        return $state;
+    }
+
+    $( '#categories_select_options' ).select2( {
+        placeholder: 'Search for Categories here',
+        delay: 250,
+        tags: true,
+        multiple: true,
+        tokenSeparators: [','],
+        minimumResultsForSearch: 1,
+        ajax: {
+            type: 'GET',
+                dataType:'json',
+                url: 'https://commons.wikimedia.org/w/api.php',
+                data: function (params) {
+                    var query = {
+                        search: params.term,
+                        action: 'opensearch',
+                        namespace: 14,
+                        format: 'json',
+                        origin: '*'
+                    }
+                    return query
+                },
+                processResults: function (data) {
+                    var processedResults = [],
+                        results = data[1];
+                    for (var i=0; i < results.length; i++) {
+                        var result = results[i];
+                        console.log(result);
+                        processedResults.push({
+                            id: result,
+                            text: result
+                        });
+                    }
+                    return {
+                        results: processedResults
+                    };
+                }
+            },
+        templateResult: categorySearchResultsFormat
+    });
 });
