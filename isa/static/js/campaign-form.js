91d1a5a3794c0c9d1fea82b1edadfb890e7dbdf1
Navino Evans <navino@histropedia.com>
2019-05-26 18:00:41 +0100

    Generate category HTML from search select on Create/Update campaign page
    This is just UI elements only. These will be checked when the form is submitted to get the JSON data to submit.
    - New table to show the selected categories
    - Function to generate the html for each category row along with it's depth control and remove button
    - Select2 search box tag mode removed, just a single selection
    - Same changes made to both Create and Update campaign templates
    Note: The old input has now been hidden, but is populated when adding new categories. This is just to keep the system working while we make the transition to JSON data in a new hidden field.
    
    Bug: T224325
    Change-Id: I48c724850a78547fe9268fbb6c7e554034a8a68e

--- isa/static/js/campaign-form.js
+++ isa/static/js/campaign-form.js
@@ -12,12 +12,9 @@ $(document).ready( function () {
         return $state;
     }
 
-    $( '#categories_select_options' ).select2( {
+    $( '#category-search' ).select2( {
         placeholder: 'Search for Categories here',
         delay: 250,
-        tags: true,
-        multiple: true,
-        tokenSeparators: [','],
         minimumResultsForSearch: 1,
         ajax: {
             type: 'GET',
@@ -38,7 +35,6 @@ $(document).ready( function () {
                         results = data[1];
                     for (var i=0; i < results.length; i++) {
                         var result = results[i];
-                        console.log(result);
                         processedResults.push({
                             id: result,
                             text: result
@@ -50,8 +46,46 @@ $(document).ready( function () {
                 }
             },
         templateResult: categorySearchResultsFormat,
-        createTag: function() {
-            return undefined;
-        }
     });
+    
+    $( '#category-search' ).on('select2:select', function(ev) {
+        var category = $(this).val();
+        addSelectedCategory(category);
+        $(this).val(null).trigger('change'); // clear the search selection 
+        $(this).select2("close"); // close the dropdown
+    })
+    
+    // Main function to add the UI elements for a new category row
+    // Used when category is added via search, or populating from existing campaign categories
+    function addSelectedCategory(name, depth) {
+        var depth = depth || 0;
+        var shortName = name.replace("Category:","");
+        $('#selected-categories-content').append(getCategoryRowHtml(shortName, depth))
+        // show the table header if it's not visible already
+        $('#selected-categories-header').show();
+        
+        // temporary! add option to old form element to keep system working while we transition to new JSON form data
+        $('#categories_select_options').append( '<option selected="selected" value="' + name + '">' + shortName + '</option>');
+    } 
+    
+    // Click event for removing categories
+    $('#selected-categories-content').on("click", "button.close", function(event) {
+        // remove the .selected-category parent container the button is within
+        $(this).closest(".selected-category").remove();
+        
+        //after removing the element, we must hide the table header if there are no rows left
+       if ( $('.selected-category').length < 1 ) {
+           $('#selected-categories-header').hide();
+       }
+    })
+    
+    // Returns the html for an individual category row, including depth option and remove button
+    function getCategoryRowHtml(name, depth) {
+        var depth = depth || 0;
+        var nameHtml = '<td class="category-name">' + name + '</td>';
+        var depthHtml = '<td> <input type="number" min="0" max="5" class="category-depth-input" value=' + depth + '> </td>';
+        var buttonHtml = '<td> <button type="button" class="close" aria-label="Close"> <span aria-hidden="true"> × </span> </button> </td>';
+        return '<tr class="selected-category">' + nameHtml + depthHtml + buttonHtml + '</tr>';
+    }
+    
 });
