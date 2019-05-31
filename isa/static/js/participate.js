2f0aa6ca9dc95208fa1eaec8400abdfac9d1d8f8
Navino Evans <navino@histropedia.com>
2019-05-31 15:05:23 +0100

    Add new client side Participation manager for image list
    -  A new constructor that is used to create a ParticipationManager object as soon as the user lands on the participate page. This manages all aspects of the participation session without reloading the page at any point.
    - The ParticipationManager has been linked to the new page structure in the previous 'Match Participate page to mock ups' commit.
    - next previous arrows setup to run new participation manager functions
    Still broken comapred to master:
    - Hard coded to a single campaign category until next commit in the series
    - Contributions are not saved
    
    Bug:T224479
    Change-Id: If4c7930fc7d93a77cc1590575f920b67edfa2bf5

--- isa/static/js/participate.js
+++ isa/static/js/participate.js
@@ -1,23 +1,45 @@
 $(document).ready( function () {
-    // we remove the auto-sliding of the carousel
-    $( '.carousel' ).carousel( {
-        interval: false
-    } );
 
-    // We hide the country sub-category input field
-    $('.country-subcategory-input').hide();
-
-    // when the page loads, we hide the section for depict search 
-    var current_slide = $( '.carousel-item' ).find( 'img' ).attr( 'alt' );
-
-    //  we write the current slide value to the captions image label
-    $( '#captions_image_label' ).val(current_slide);
-    $( '#captions_image_label' ).hide();
-    // we get the language set for the depicts
-    var depicts_language = $('#depicts_lang_select').val();
-
-    // we convert the depicts seach box to a select field
-    // which will make the Ajax call to Wikidata api
+    // Retrieve the list of images from the Commons API
+    // TODO: HARD CODED temporary category query - will be replaced with:
+    // - getCapaignCategories(doneCallback, thenCallback)
+    // - .done getSubcategories(categoriesArray) - asynch gets unique list of all subcategories for each campaign category (to specified depth). Includes the root categories as well
+    // - .then getImagesFromCategories(allCatsAndSubcats, allCompleteCallback) - async request images for all subcategories and 
+    // Note: both categorymember API calls will use generalised API call function, with cmtype=subcat / cmtype=file
+    
+    var apiOptions = {
+        action: 'query',
+        list: 'categorymembers',
+        cmtitle: 'Category:Images from Wiki Loves Africa 2019',
+        cmlimit: 'max',
+        cmtype: 'file',
+        format: 'json',
+        origin: '*'
+    };
+    
+    $.ajax({
+        type: 'GET',
+        url: 'https://commons.wikimedia.org/w/api.php',
+        data: apiOptions
+    }).done(function(response) {
+        var results = response.query.categorymembers;
+        var images = results.map(function(result) {
+            var filename = result.title;
+            return result.title;
+        }).filter(function(filename) {
+            // only include filenames with supported extensions
+            var fileExtension = filename.split('.').pop().toLowerCase();
+            var allowedExtensions = ["jpg", "jpeg", "png", "svg"];
+            return allowedExtensions.includes(fileExtension);
+        })
+        
+        // launch new participationManager using list of images in response from api
+        window.participationManager = new ParticipationManager(images);
+        participationManager.imageChanged();
+    })
+    
+    // Initialise the depicts search box
+    // Results populated via api call
 
     function searchResultsFormat(state) {
         if (!state.id) {
@@ -29,8 +51,8 @@ $(document).ready( function () {
         return $state;
       }
   
-      function setUpDepictsForm(current_image){
-          $( '#depicts_select_options' ).select2( {
+    (function setUpDepictsSearch(){
+          $( '#depicts-select' ).select2( {
               placeholder: 'Search for depicts',
               delay: 250,
               tags: true,
@@ -46,7 +68,7 @@ $(document).ready( function () {
                       var query = {
                           search: params.term,
                           action: 'wbsearchentities',
-                          language: depicts_language,
+                          language: 'en',
                           format: 'json',
                           uselang: 'en',
                           origin: '*'
@@ -74,151 +96,192 @@ $(document).ready( function () {
                 return undefined;
             }
         });
-      }
-/**
-	 * Populate images metadata.
-	 *
-	 * @param {String} filename - the name of the file.
-	 */
-    function populateImageMetadata( filename ) {
-        var apiOptions = {
-            action: 'query',
-            titles: filename,
-            prop: 'imageinfo',
-            iiprop: 'extmetadata',
-            format: 'json',
-            formatversion: 2,
-            origin: '*'
-        };
+      })();
 
-        $.ajax( {
-            type: 'GET',
-            url: 'https://commons.wikimedia.org/w/api.php',
-            data: apiOptions
-        } )
-        .done( function( response ) {
-            var response_metadata = response.query.pages[0].imageinfo[0].extmetadata;
-            var htmlStrippedDescription = $('<span>' + response_metadata.ImageDescription.value + '</span>').text();
-            $( '#image_name' ).text( response.query.pages[0].title );
-            $( '#image_description' ).html( htmlStrippedDescription );
-            $( '#image_categories' ).text( response_metadata.Categories.value );
-            $( '#image_author' ).html( response_metadata.Artist.value );
-        } );
-    }
 
-    	/**
-	 * Populate images structured data.
-	 *
-	 * @param {String} filename - the name of the file.
-	 */
-    function populateStructuredDataSection( filename ) {
-        var entitiesApiOptions = {
-            action: 'wbgetentities',
-            titles: filename,
-            sites: 'commonswiki',
-            format: 'json',
-            formatversion: 2,
-            origin: '*'
-        };
-        $.ajax( {
-            type: 'GET',
-            url: 'https://commons.wikimedia.org/w/api.php',
-            data: entitiesApiOptions
-        } ).done( function( response ) {
-            var m_number = Object.keys(response.entities)[0];
-            var depictsStatements = {};
-
-            if ( response.entities[m_number].statements === undefined ) {
-                $( '#depicts_columns' ).empty();
-                $( '#depicts_error_message' ).empty();
-                $('#depicts_error_message').show();
-                $( '#depicts_error_message' ).append( "<div class='text-center col-md-12 pt-0 pb-0 mr-auto'>" +
-                    "<small>This image has no depicts</small></div>" );
-            } else {
-                depictsStatements = response.entities[m_number].statements.P180;
-            }
-            var wikidata_Q_values = [];
-            for (let index = 0; index < depictsStatements.length; index++) {
-                wikidata_Q_values.push(depictsStatements[index].mainsnak.datavalue.value.id);
-            }
-            var first_element =  wikidata_Q_values.pop(0);
-            var split_array = wikidata_Q_values.join('|');
-            var id_string = '';
-            if ( wikidata_Q_values.length === 0 ) {
-                id_string = first_element;
-            } else {
-                id_string = first_element + '|' + split_array;
-            }
-            // we make an api call and get depicts for this id
-            var secondApiOptions = {
-                action: 'wbgetentities',
-                props: 'labels|descriptions',
-                format: 'json',
-                ids: id_string,
-                languages: 'en|fr',
-                formatversion: 2,
-                origin: '*',
-                languagefallback: ''
-            };
-            $.ajax( {
-                type: 'GET',
-                url: 'https://www.wikidata.org/w/api.php',
-                data: secondApiOptions
-                })
-            .done( function ( response ) {
-                // $( '#depicts_columns' ).empty();
-                $('#depicts_error_message').show();
-                for ( var qvalue in response.entities ) {
-                    for ( var lang in response.entities[qvalue].labels) {
-                        $( '#depicts_columns' ).append(
-                            "<div class='col-sm-2 mr-auto'>" +
-                            "<div class='chip lighten-1><i class='close fas fa-times'>" +
-                            response.entities[qvalue].labels[lang].value + "</i></div>" +
-                            "</div>"
-                        ); 
-                    }
-                }
-            } );
-        } );
-    }
-    $( '#depicts_columns' ).empty();
-    //  Populate  metadata for current slide
-    // In this case, the carousel-item has not changed yet
-
-    populateImageMetadata( current_slide );
-    populateStructuredDataSection( current_slide );
-    setUpDepictsForm(current_slide);
-    // here we detect a change in the carousel item
-    $( '#campaignPartcipateImageCarousel' ).bind( 'slide.bs.carousel', function (e) {
-        $( '#depicts_columns' ).empty();
-        $( '#depicts_error_message' ).empty();   
-        // we get the previous slide
-        
-        var slideFrom = $(this).find( '.active' ).index();
-        var totalItems = $( '.carousel-item' ).length;
-        if( slideFrom < totalItems - 1 ) {
-            var destination = $( '.carousel-item' ).eq( slideFrom + 1 ).find( 'img' ).attr( 'alt' );
-            //  we update the captions_image label
-            $( '#captions_image_label' ).val(destination);
-            $( '#captions_image_label' ).hide();
-            populateImageMetadata( destination );
-            populateStructuredDataSection( destination );
-            setUpDepictsForm( destination );
-        }
-        else{
-            var current_slide = $( '.carousel-item' ).find( 'img' ).attr( 'alt' );
-            //  we update the captions_image label
-            $( '#captions_image_label' ).val(current_slide);
-            $( '#captions_image_label' ).hide();
-            populateImageMetadata( current_slide );
-            populateStructuredDataSection( current_slide );
-            setUpDepictsForm( current_slide );
-        }
-    } );
-
-    $( '#depicts_search_form_section' ).slideUp();
     $( '#add_depict_btn' ).click(function (e) {
         $( '#depicts_search_form_section' ).slideDown();
         return false;
     });
+    
+    
+    // ParticipationManager class manages all aspects of the current participation session
+    // The initialData property contains an object with inital depicts and captions data retrieved from Commons
+    // Difference between current and initialData is used to determine unsavedChanges after each change made in UI 
+    //  - This allows us to update "publish" and "cancel" button states
+    //  - Also used for sending edits to Commons and ISA database when either submit is click 
+    // Data is sent via ajax post request instead of default form submit to prevent page reload
+    ParticipationManager = function(images) {
+        var imageIndex = 0,
+            countrySubcategory = getUrlParameters().country, //returns country or undefined
+            initialData = {depicts: [], captions: []},
+            unsavedChanges = {depicts: [], captions: []};
+        
+        this.nextImage = function() {
+            imageIndex = (imageIndex + 1) % (images.length);
+            this.imageChanged();
+        }
+        
+        this.previousImage = function() {
+            imageIndex -= 1;
+            //jump to end of list if previous image is called on index=0
+            if (imageIndex < 0) imageIndex = images.length - 1;
+            this.imageChanged();
+        }
+        
+        this.imageChanged = function() {
+            // all actions to complete when a new image has loaded
+            var file = getImageFilename()
+            updateImage(file);
+            populateMetadata(file);
+            populateStructuredData(file, /*callback*/ saveInitialStructuredData);
+        }
+    
+        function getImageFilename () {
+            return images[imageIndex];
+        }
+        
+        function saveInitialStructuredData(depictsData, captionsData) {
+            // todo: store the results of the intial api call, so represents the live data before any changes
+            initialData.depicts = depictsData;
+            //initialData.captions = captionsData;
+        }
+        
+        function updateUnsavedChanges() {
+            // todo: compare current selection to initialData to see if there are any unsaved changes
+        }
+        
+    
+        function updateImage(filename) {
+            $('.img-holder img').attr('src', 'https://commons.wikimedia.org/wiki/Special:FilePath/' + filename + '?width=500')
+        }
+        
+        function populateMetadata(filename) {
+            var apiOptions = {
+                action: 'query',
+                titles: filename,
+                prop: 'imageinfo',
+                iiprop: 'extmetadata',
+                format: 'json',
+                formatversion: 2,
+                origin: '*'
+            };
+
+            $.ajax( {
+                type: 'GET',
+                url: 'https://commons.wikimedia.org/w/api.php',
+                data: apiOptions
+            } )
+            .done( function( response ) {
+                var metadata = response.query.pages[0].imageinfo[0].extmetadata;
+                var title = response.query.pages[0].title;
+                var escapedTitle = encodeURIComponent(title);
+                var htmlStrippedDescription = $('<span>' + metadata.ImageDescription.value + '</span>').text();
+                $('#image_name').html('<a href=' + "https://commons.wikimedia.org/wiki/" + escapedTitle + ' target="_blank">' + title.replace("File:", "") + '</a>');
+                $('#image_description').html(htmlStrippedDescription);
+                $('#image_categories').text(metadata.Categories.value);
+                $('#image_author').html(metadata.Artist.value);
+            } );
+        }
+
+        function populateStructuredData(filename, callback) {
+            $('.depict-tag-group').empty(); //clear previous
+            var entitiesApiOptions = {
+                action: 'wbgetentities',
+                titles: filename,
+                sites: 'commonswiki',
+                format: 'json',
+                origin: '*'
+            };
+            $.ajax( {
+                type: 'GET',
+                url: 'https://commons.wikimedia.org/w/api.php',
+                data: entitiesApiOptions
+            } ).done( function( response ) {
+                // todo: get captions data, and pass cleaned data to callback function (which saves initial state)
+                var mediaId = Object.keys(response.entities)[0];
+                var depictsStatements = [];
+                // no depicts data
+                // Todo: add "this item has no depicts statement yet" message to separate div
+
+                var mediaStatements = response.entities[mediaId].statements;
+                if ( !(mediaStatements && mediaStatements.P180) ) {
+                    //todo: add message to new div
+                    return console.log("this item has no depicts statements yet")
+                }
+
+                // convert results to list of Q numbers only
+                // todo: switch to array of objects to store isProminent value for each 
+                depictsItems = mediaStatements.P180.map(function(depictsStatement) {
+                    return depictsStatement.mainsnak.datavalue.value.id;
+                });
+                if (callback) callback(depictsItems);
+                if (depictsItems.length === 0) return;
+                
+                // now make another call to Wikidata to get the labels for each depcits item
+                var secondApiOptions = {
+                    action: 'wbgetentities',
+                    props: 'labels|descriptions',
+                    format: 'json',
+                    ids: depictsItems.join("|"),
+                    languages: 'en',
+                    origin: '*',
+                    languagefallback: ''
+                };
+                $.ajax( {
+                    type: 'GET',
+                    url: 'https://www.wikidata.org/w/api.php',
+                    data: secondApiOptions
+                    })
+                .done( function (response) {
+                    // now we have the labels, populate the statements area to show existing depicts items
+                    var intialStatementsHtml = "";
+                    for (var qvalue in response.entities) {
+                        var itemData = response.entities[qvalue],
+                            labelLang = Object.keys(itemData.labels)[0],
+                            label = itemData.labels[labelLang].value,
+                            descriptionLang = Object.keys(itemData.descriptions)[0],
+                            description = itemData.descriptions[descriptionLang].value;
+                            //todo: access isProminent value
+                        intialStatementsHtml += getStatementHtml(qvalue, label, description);
+                    }
+                    $('.depict-tag-group').html(intialStatementsHtml);
+                });
+            });
+        }
+        
+        function getUrlParameters () {
+            var parametersObject = {};
+            var parameters = window.location.search.substr(1);
+            if (parameters == "") return {};
+            parameters = parameters.split('&');
+            for (var i = 0; i < parameters.length; i++) {
+                var splitParameters = parameters[i].split('=');
+                parametersObject[ splitParameters[0] ] = splitParameters[1];
+            }
+            return parametersObject;
+        };
+        
+        function getStatementHtml(item, label, description, isProminent) {
+            return [
+                '<div class="depict-tag-item" title="' + description + '">',
+                '<div class="depict-tag-label">', 
+                '<div class="label btn-sm">' + label + ' <span class="depict-tag-item">' + item + '</span></div>',
+                '<button class="btn btn-sm btn-warning mark-prominent-btn" title="Mark this depicted item as NOT prominent">Prominent</button>',
+                '<div class="depict-tag-btn">',
+                '<button class="fas fa-trash btn-link btn" title="Remove this depicted item"></button></div>',
+                '</div></div></div>'].join("");
+        }
+    }
+    
+    /********* click handlers *********/
+    $('#next-image-btn').click(function(ev) {
+        participationManager.nextImage();
+    })
+    
+    $('#previous-image-btn').click(function(ev) {
+        participationManager.previousImage();
+    })
+    
 });
+    
