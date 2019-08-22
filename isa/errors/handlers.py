d257c7beb09494ee44e1bc85bbf94076d21e8fa4
Egbe Eugene <agboreugene@gmail.com>
2019-08-22 11:50:47 +0100

    Add session language to error page routes
    * Added session_language to error handlers route data
    
    Bug: T230996
    Change-Id: I76c884a747d76b9bdff6115ad37e5f56ec6398b8

--- isa/errors/handlers.py
+++ isa/errors/handlers.py
@@ -1,18 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 
 errors = Blueprint('errors', __name__)
 
 
 @errors.app_errorhandler(404)
 def error_404(error):
-    return render_template('errors/404.html'), 404
+    session_language = session.get('lang', None)
+    if not session_language:
+        session_language = 'en'
+    return render_template('errors/404.html',
+                           title='ISA-404',
+                           session_language=session_language), 404
 
 
 @errors.app_errorhandler(403)
 def error_403(error):
-    return render_template('errors/403.html'), 403
+    session_language = session.get('lang', None)
+    if not session_language:
+        session_language = 'en'
+    return render_template('errors/403.html',
+                           title='ISA-403',
+                           session_language=session_language), 403
 
 
 @errors.app_errorhandler(500)
 def error_500(error):
-    return render_template('errors/500.html'), 500
+    session_language = session.get('lang', None)
+    if not session_language:
+        session_language = 'en'
+    return render_template('errors/500.html',
+                           title='ISA-500',
+                           session_language=session_language), 500
