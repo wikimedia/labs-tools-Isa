ae80d9a19e4adef89f492dde2f0b12c39ae02a1b
Egbe Eugene <agboreugene@gmail.com>
2019-10-08 15:12:46 +0100

    Add unit test for `convert_latin_to_english()` function
    Added test to convert text with special characters to English
    
    Bug: T234927
    Change-Id: I11652bcb0863e4680e2c7f8011a4bbc8d22cfbcd

--- tests/test_campaign_utils.py
+++ tests/test_campaign_utils.py
@@ -5,19 +5,22 @@
 
 import unittest
 
-from isa.campaigns.utils import get_country_from_code
+from isa.campaigns.utils import get_country_from_code, convert_latin_to_english
 
 
 class TestCampaignUtils(unittest.TestCase):
     def setUp(self):
         pass
-    
+
     def tearDown(self):
         pass
-    
+
     def test_get_country_from_code(self):
         country = get_country_from_code('CM')
         self.assertEqual(country, 'Cameroon')
+
+    def test_convert_latin_to_english(self):
+        self.assertEqual(convert_latin_to_english('Třebíč'), 'Trebic')
 
 
 if __name__ == '__main__':
