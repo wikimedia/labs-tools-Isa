bb46c7929ed1393a02e6b435f982404ad7a40f3e
Egbe Eugene <agboreugene@gmail.com>
2019-10-08 11:02:28 +0100

    Add unit test for `compute_campaign_status()` function
    Wrote test for compute_campaign_status utility function in campaign blueprint
    
    Bug: T234896
    Change-Id: If24df8e3e564f97a33908a5a25854bfe15d64b7d

--- tests/test_campaign_utils.py
+++ tests/test_campaign_utils.py
@@ -4,11 +4,14 @@
 # Unit tests for the utility functions in the campaign blueprint
 
 import unittest
+import datetime
 
-from isa.campaigns.utils import get_country_from_code, convert_latin_to_english
+from isa.campaigns.utils import get_country_from_code, convert_latin_to_english, compute_campaign_status
 
 
 class TestCampaignUtils(unittest.TestCase):
+    """Test utility functions in the campaign blueprint."""
+
     def setUp(self):
         pass
 
@@ -21,6 +24,12 @@ class TestCampaignUtils(unittest.TestCase):
 
     def test_convert_latin_to_english(self):
         self.assertEqual(convert_latin_to_english('Třebíč'), 'Trebic')
+
+    def test_compute_campaign_status(self):
+        date_time_str = '2018-06-29 08:15'
+        end_date = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M')
+        campaign_status = compute_campaign_status(end_date)
+        self.assertTrue(campaign_status)
 
 
 if __name__ == '__main__':
