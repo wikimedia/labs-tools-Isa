e91a07b125c6d8b07a9e6ea9e3c2d338afd19489
Egbe Eugene <agboreugene@gmail.com>
2019-10-08 14:35:58 +0100

    Write unittest for get_all_camapign_stats_data function
    * Added test for get_all_camapign_stats_data utility function of the campaign blueprint
    
    Bug: T234922
    Change-Id: I2b75580e11e99ab754e47acd15de50464818ea23

--- tests/test_campaign_utils.py
+++ tests/test_campaign_utils.py
@@ -6,7 +6,10 @@
 import unittest
 import datetime
 
-from isa.campaigns.utils import get_country_from_code, convert_latin_to_english, compute_campaign_status
+from isa.campaigns.utils import (get_country_from_code, convert_latin_to_english, compute_campaign_status,
+                                 get_all_camapign_stats_data)
+
+from isa.models import Contribution, Campaign
 
 
 class TestCampaignUtils(unittest.TestCase):
@@ -30,6 +33,17 @@ class TestCampaignUtils(unittest.TestCase):
         end_date = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M')
         campaign_status = compute_campaign_status(end_date)
         self.assertTrue(campaign_status)
+
+    def test_get_all_camapign_stats_data(self):
+        campaign_stats_users = []
+        contribution_users = []
+        campaign_contributions = Contribution.query.filter_by(campaign_id=1).all()
+        all_campaign_stats_data = get_all_camapign_stats_data(1)
+        for contribution in campaign_contributions:
+            contribution_users.append(contribution.username)
+        for stats in all_campaign_stats_data:
+            campaign_stats_users.append(stats['username'])
+        self.assertEqual(campaign_stats_users, contribution_users)
 
 
 if __name__ == '__main__':
