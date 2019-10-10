04a708da4ba974f843d312833dd2c4b5000aa1a4
Egbe Eugene <agboreugene@gmail.com>
2019-10-10 11:12:52 +0100

    Unit test for getCampaigns route in the campaign blueprint
    Add unit test for getCampaigns() route in the campaign blueprint
    
    Bug: T235158
    Change-Id: I9522e0b4281ba4b6da5b205f97200e09f860c1da

---
+++ tests/test_campaign_routes.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Author: Eugene Egbe
+# Unit tests for the routes in the isa tool
+
+import unittest
+
+from flask import session
+
+from isa import app
+
+
+class TestCampaignRoutes(unittest.TestCase):
+    # setup and teardown #
+
+    # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+
+    # executed after each test
+    def tearDown(self):
+        pass
+
+    # tests #
+
+    def test_get_campaigns_route(self):
+        response = self.app.get('/campaigns', follow_redirects=True)
+        self.assertEqual(response.status_code, 200,)
+
+
+if __name__ == '__main__':
+    unittest.main()
